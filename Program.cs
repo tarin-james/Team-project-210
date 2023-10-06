c8af99422f5107703ae0857a36f73ab91af2b969
Tarin James <122317277+tarin-james@users.noreply.github.com>
2023-10-06 17:11:16 -0600

    my soul is finally free

--- Program.cs
+++ Program.cs
@@ -4,9 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        SaveAndLoad saveAndLoadHandler = new SaveAndLoad();
-        saveAndLoadHandler.Save("teej.txt", "wowzerjkjjk");
-        string output = saveAndLoadHandler.Load("teej.txt");
-        Console.WriteLine(output);
+        Menu menuDisplay = new Menu();
+        menuDisplay.DisplayMenu();
+
     }
 }
--- SaveAndLoad.cs
+++ SaveAndLoad.cs
@@ -2,24 +2,42 @@ using System.IO;
 
 class SaveAndLoad
 {
-    public string Load(string fileName) {
-        string[] lines = System.IO.File.ReadAllLines(fileName);
-        string output = "";
-        foreach (string line in lines)
+    public List<Entry> Load() {
+        Console.WriteLine("What is the file name? ");
+        string userFileName = Console.ReadLine();
+
+        string[] lines = System.IO.File.ReadAllLines(userFileName);
+        List<Entry> loadedEntries = new List<Entry> {};  
+        foreach(string line in lines)
         {
-            output += line;
+            if (line.Length > 0) {
+                string[] entryAttributes = line.Split("~");
+                Entry newEntry = new Entry();
+                newEntry.date = entryAttributes[0];
+                newEntry.prompt = entryAttributes[1];
+                newEntry.response = entryAttributes[2];
+
+                loadedEntries.Add(newEntry);
+            }
+        
+        } 
+        return loadedEntries;
+    }
+
+    public void Save(List<Entry> entriesToSave) { 
+        Console.WriteLine("What is the file name? ");
+        string userFileName = Console.ReadLine(); 
+        string entriesToSaveString = "";
+        foreach(Entry entryToSave in entriesToSave) {
+            entriesToSaveString += $"{entryToSave.date}~{entryToSave.prompt}~{entryToSave.response}\n";
         }
-        return output;
-    }
 
-    public void Save(string fileName, string journalEntry) {
+        using (StreamWriter outputFile = new StreamWriter(userFileName))
 
-        using StreamWriter outputFile = new StreamWriter(fileName);
-
-        outputFile.WriteLine(journalEntry);
-
-    }
+         
+        outputFile.WriteLine(entriesToSaveString);
     
-     
+    }
+
     
 }
